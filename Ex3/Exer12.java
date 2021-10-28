054acca033bc7030debcb3506a1ddc1c0aaf0c43
RubensGabrielVelazquezFonseca <55161653+RubensGabrielVelazquezFonseca@users.noreply.github.com>
2021-10-27 22:00:29 -0300

    Add files via upload

---
+++ Ex3/Exer12.java
@@ -0,0 +1,21 @@
+
+package exer3;
+
+import java.util.Scanner;
+
+public class Exer12 {
+    
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Entre com o número para gerar a tabuada:");
+        int num = scan.nextInt();
+        
+        System.out.println("Tabuada de " + num + ":");
+        
+        for (int i=1; i<=10; i++){
+            System.out.println(num + " x " + i + " = " + (num*i));
+        }
+    }
+}
---
+++ Ex3/Exer23.java
@@ -0,0 +1,18 @@
+
+package exer3;
+import java.util.Scanner;
+
+
+public class Exer23{
+    
+    public static void main(String[] args){
+        
+       // Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Lojas Quase Dois - Tabela de preços");
+        
+        for (int i=1; i<=50; i++){
+            System.out.println(i + " - R$ " + (1.99*i));
+        }
+    }
+}
