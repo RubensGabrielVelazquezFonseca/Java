0d0c9f6d1b3e7ceb5c6ea51fe1b22f08c5fc2f6c
RubensGabrielVelazquezFonseca <55161653+RubensGabrielVelazquezFonseca@users.noreply.github.com>
2021-11-05 08:59:40 -0300

    2

---
+++ trabalho_playlist/viideo.java
@@ -0,0 +1,43 @@
+public class Viideo {
+  private String titulo;
+	private String url;
+	private Integer minutos;
+	
+	
+	public Viideo(String _titulo, String _url, Integer _minutos) {
+		this.titulo = _titulo;
+		this.url = _url;
+		this.minutos = _minutos;
+	}
+	// construtor vazio
+	public Viideo() {
+	}
+	
+	@Override
+	public String toString() {		
+		return "Titulo: " + this.titulo;
+	}
+	
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public Integer getMinutos() {
+		return minutos;
+	}
+	public void setMinutos(Integer minutos) {
+		this.minutos = minutos;
+	}
+	
+	
+
+}
