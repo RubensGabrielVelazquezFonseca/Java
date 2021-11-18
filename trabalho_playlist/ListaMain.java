ff63d6387f8c176c62ca2fbb0c3e07a261b5570c
RubensGabrielVelazquezFonseca <55161653+RubensGabrielVelazquezFonseca@users.noreply.github.com>
2021-11-18 17:50:15 -0300

    listmain

---
+++ trabalho_playlist/ListaMain.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaMain {
+	
+	List<Playlist> lista = new ArrayList<>();
+
+	public static void main(String[] args) {
+		
+		
+		//listas das listas
+		ListaMain listaMain = new ListaMain();
+		
+		
+		//criar as playlist
+		
+		
+		
+		
+		
+	
+
+	}
+
+}
