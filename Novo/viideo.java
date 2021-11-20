c5354605cbf24deb17b7b3e9b060f1e14f9f4322
RubensGabrielVelazquezFonseca <55161653+RubensGabrielVelazquezFonseca@users.noreply.github.com>
2021-11-20 11:39:59 -0300

    Create viideo.java

---
+++ Novo/viideo.java
@@ -0,0 +1,55 @@
+public class Viideo {
+	  private String titulo;
+		private String url;
+		private Integer minutos;
+	    private boolean assistido;
+
+		
+		public Viideo(String _titulo, String _url, Integer _minutos) {
+			this.titulo = _titulo;
+			this.url = _url;
+			this.minutos = _minutos;
+	        this.assistido = assistido;
+
+		}
+		public Viideo() {
+		}
+		
+		@Override
+		public String toString() {		
+	 return " Titulo: " + this.titulo + " / url:  " + this.url + " /  Minutos:  " + minutos  +"/ assistido=" + assistido;
+
+
+		}
+		
+		
+		public String getTitulo() {
+			return titulo;
+		}
+		public void setTitulo(String titulo) {
+			this.titulo = titulo;
+		}
+		public String getUrl() {
+			return url;
+		}
+		public void setUrl(String url) {
+			this.url = url;
+		}
+		public Integer getMinutos() {
+			return minutos;
+		}
+		public void setMinutos(Integer minutos) {
+			this.minutos = minutos;
+		}
+		
+
+	    public boolean isAssistido() {
+	        return assistido;
+	    }
+
+	    public void setAssistido(boolean assistido) {
+	        this.assistido = assistido;
+	    }
+		
+
+	}
