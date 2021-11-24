5b0dde6ee0dcbe8302a72b6926cc116394d14e12
RubensGabrielVelazquezFonseca <55161653+RubensGabrielVelazquezFonseca@users.noreply.github.com>
2021-11-24 19:34:56 -0300

    Update programa2.java

--- Notas/programa2.java
+++ Notas/programa2.java
@@ -1,57 +1,41 @@
 /*Programa 2:
-
 Conceito médio: Agora, vamos calcular o conceito médio considerando a seguinte relação entre conceito e valor:
 A= 10
 B= 7.5
 C=6
 D=4
-
 Adicionar os seguintes conceitos para o aluno: A, B, D, D, A
-
 Faça no código um método que calcula a média final fornecidos os conceitos acima. Obs: cada conceito é convertido em uma nota, e depois calculamos a média. A média final (número) é convertida para um conceito.
 Exibir a informação para o usuário.*/
 
 
 
-import java.util.Scanner;
+package Dp2;
+public class programa2 {
 
-public class Programador2 {
-    
     public static void main(String[] args) {
-        Scanner ent = new Scanner(System.in);
-        int nota1, nota2, nota3, nota4;
-        int media, i, contAluno = 0;
         
-        for(i = 0; i < 4; i++){
-            
-            contAluno++;
-            System.out.println("Aluno " + contAluno + ", digite sua 1ª nota");
-            nota1 = ent.nextInt();
-            
-            System.out.println("Aluno " + contAluno + ", digite sua 2ª nota");
-            nota2 = ent.nextInt();
-            
-            System.out.println("Aluno " + contAluno + ", digite sua 3ª nota");
-            nota3 = ent.nextInt();
-            
-               System.out.println("Aluno " + contAluno + ", digite sua 4ª nota");
-            nota4 = ent.nextInt();
-            
-            
-            media = (nota1 + nota2 + nota3 + nota4) / 4;
-            System.out.println("A média do aluno " + contAluno + " é " + media);
-            
-            if( (media >= 0) && (media <4) ){
-                System.out.println("Nota E");
-            } else if(media < 5){
-                System.out.println("Nota D");
-            } else if(media < 7){
-                System.out.println("Nota C");
-            } else if(media < 8){
-                System.out.println("Nota B");
-            } else if(media <= 10){
-                System.out.println("Nota A");
-            }
-        }       
+         Conceito a1 = new Conceito("A");
+        Conceito b1 = new Conceito("B");
+        Conceito c1 = new Conceito("C");
+        Conceito d1 = new Conceito("D");
+        
+        // conceitos.add(a1);
+        
+        System.out.println("Valor de a1: " + 
+                a1.letra + " == " + a1.getNota());
+        
+        System.out.println("Valor de b1: " + 
+                b1.letra + " == " + b1.getNota());
+        
+         System.out.println("Valor de c1: " + 
+                c1.letra + " == " + c1.getNota());
+        
+          System.out.println("Valor de d1: " + 
+                d1.letra + " == " + d1.getNota());
+        
     }
+
 }
+        
+        
