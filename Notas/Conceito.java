faf05cc4bd08f40e725947e3abeddd4038bfd1a9
RubensGabrielVelazquezFonseca <55161653+RubensGabrielVelazquezFonseca@users.noreply.github.com>
2021-11-24 10:13:04 -0300

    Create Conceito.java

---
+++ Notas/Conceito.java
@@ -0,0 +1,20 @@
+public class Conceito {
+	
+	// A, B, C, D
+	public String letra; 
+	
+	public Conceito(String _letra) {
+		letra = _letra;
+	}
+	
+	public Double getNota() {
+		if (this.letra == "A")
+			return 10d; // d = double
+		if (this.letra == "B")
+			return 8d;
+		if (this.letra == "C")
+			return 6d;
+		return 4d;	// D	
+	}
+
+}
