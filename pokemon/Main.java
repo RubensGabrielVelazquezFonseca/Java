b1f4891385b9ead5ee5fae4476287826d25bf010
RubensGabrielVelazquezFonseca <55161653+RubensGabrielVelazquezFonseca@users.noreply.github.com>
2022-02-12 23:38:26 -0300

    Create Main.java

---
+++ pokemon/Main.java
@@ -0,0 +1,24 @@
+package pokemon;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Pokemon pikachu = new Pokemon("Elétrico", 100, 10, 2);
+                Pokemon bulbasaur = new Pokemon("Grama", 80, 8, 3);
+
+		
+		pikachu.pokeSetup("Pikachu");
+                pikachu.pokeSetup("bulbasaur");
+                
+                
+                
+                
+                    
+                
+                
+                
+                
+
+	}
+}
