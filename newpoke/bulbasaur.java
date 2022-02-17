062193a59c87b74f05895c5dd64c8a288b388d8b
RubensGabrielVelazquezFonseca <55161653+RubensGabrielVelazquezFonseca@users.noreply.github.com>
2022-02-16 21:07:53 -0300

    Create bulbasaur.java

---
+++ newpoke/bulbasaur.java
@@ -0,0 +1,22 @@
+public class bulbasaur implements PokeInterface  {
+
+    @Override
+    public void getAtaque() {
+    }
+
+    @Override
+    public void setAtaque() {
+    }
+
+    @Override
+    public void getDefesa() {
+    }
+
+    @Override
+    public void setDefesa() {
+    }
+    
+    
+    
+    
+}
