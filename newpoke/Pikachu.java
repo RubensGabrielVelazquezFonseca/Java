1e0da8a1bce5995392e68126b66d7b2933b50a5f
RubensGabrielVelazquezFonseca <55161653+RubensGabrielVelazquezFonseca@users.noreply.github.com>
2022-02-16 21:07:28 -0300

    Create Pikachu.java

---
+++ newpoke/Pikachu.java
@@ -0,0 +1,24 @@
+public class Pikachu implements PokeInterface {
+
+    @Override
+    public void getAtaque() {
+    }
+
+    @Override
+    public void setAtaque() {
+    }
+
+    @Override
+    public void getDefesa() {
+    }
+
+    @Override
+    public void setDefesa() {
+    }
+    
+    
+    
+    
+    
+}
+
