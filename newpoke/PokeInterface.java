9501e101c06438815af52330aead5eda874e540f
RubensGabrielVelazquezFonseca <55161653+RubensGabrielVelazquezFonseca@users.noreply.github.com>
2022-02-16 21:07:03 -0300

    Create PokeInterface.java

---
+++ newpoke/PokeInterface.java
@@ -0,0 +1,11 @@
+public interface PokeInterface {
+     public void getAtaque();
+     public void setAtaque();
+     public void getDefesa();
+     public void setDefesa();
+
+
+     
+    
+    
+}
